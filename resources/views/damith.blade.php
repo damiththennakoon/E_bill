8c9af6e1f34a2d39699372a5b546bf47df19c94b
Shehan Rashmika <sarawalkthrough@gmail.com>
2025-01-13 00:06:16 +0530

    add routes and views for damith and test pages

---
+++ resources/views/damith.blade.php
@@ -0,0 +1,36 @@
+<html>
+
+<head>
+    <title>Test</title>
+    <style>
+        .damith {
+            color: red;
+            font-size: 200px;
+        }
+
+        .bhagya {
+            background-color: yellow
+        }
+    </style>
+</head>
+
+<body>
+    <h1 class="damith bhagya">THis is test page</h1>
+    <p class="bhagya">This is a Moda damith</p>
+    <a href="http://e_bill.test/test">click me</a>
+    <h1>dfgdfg</h1>
+    <h2>dfsfdsdf</h2>
+    <h3 class="damith">Moda thisara</h3>
+
+    <p>fghfghfgh</p>
+    <a href="dfg">dfgdfg</a>
+    <button>CLick me</button>
+    <label for="">User name</label>
+    <input type="text">
+    <img src="" alt="">
+
+
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/damith', function () {
+    return view('damith');
+});
+
+Route::get('/test', function () {
+    return view('test');
+});
